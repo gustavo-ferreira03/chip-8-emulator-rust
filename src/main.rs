160fa5bc2ac3d9c8c216a986bee4b84ad2c791c0
Gustavo Ferreira <gustavo_ferreiracosme03@protonmail.com>
2023-03-07 14:47:43 -0300

    feat: add addition operation

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,24 @@ struct Cpu {
     current_operation: u16,
 }
 
+impl Cpu {
+    fn run(&mut self) {
+        let hh = (self.current_operation & 0xF000) >> 12;
+        let hl = (self.current_operation & 0x0F00) >> 8;
+        let lh = (self.current_operation & 0x00F0) >> 4;
+        let ll = self.current_operation & 0x000F;
+
+        match (hh, hl, lh, ll) {
+            (8, _, _, 4) => {
+                self.registers[hl as usize] = self.registers[hl as usize] + self.registers[lh as usize];
+            },
+            _ => {
+                
+            }
+        }
+    }
+}
+
 fn main() {
     let mut cpu = Cpu {
         registers: [0; 0xF],
@@ -13,5 +31,6 @@ fn main() {
     cpu.registers[1] = 3;
     cpu.current_operation = 0x8014;
 
+    cpu.run();
     println!("{}", cpu.registers[0]);
 }
